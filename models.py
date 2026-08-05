b2d1dbb9ac061557d990cd2ce15b5349cbd7f1d5
kabaratachristopher <kabaratachristopher2@gmail.com>
2026-08-05 15:43:57 +0300

    Fixed password_hash length for PostgreSQL

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(500), nullable=False)
     role = db.Column(db.String(20), nullable=False)
 
 
